42796e08a66eaf140cc12d076f2273f850e77b64
HanYing LIN <linhanying@xman.studio>
2019-05-31 23:57:40 +0800

    Deploy

---
+++ src/main/java/com/pku/hyl/abouthilary/AboutHilaryApplication.java
@@ -0,0 +1,13 @@
+package com.pku.hyl.abouthilary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboutHilaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AboutHilaryApplication.class, args);
+    }
+
+}
